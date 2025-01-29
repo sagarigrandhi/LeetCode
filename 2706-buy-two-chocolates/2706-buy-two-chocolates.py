6218e71b6b23968a67c6c16779cfaa3707a86696
Ratna Sagari Grandhi <sagari.grandhi@gmail.com>
2025-01-28 23:53:50 -0500

    Delete 2706-buy-two-chocolates/2706-buy-two-chocolates.py

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def buyChoco(self, prices: List[int], money: int) -> int:
-        prices.sort()
-        summed_prices = prices[0] + prices[1]
-        return money - summed_prices if summed_prices <= money else money
-        
