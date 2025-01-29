546a54192dc3a12e48d99f94857094b60fb92656
Ratna Sagari Grandhi <sagari.grandhi@gmail.com>
2025-01-28 23:51:05 -0500

    Time: 0 ms (100%), Space: 17.9 MB (24.98%) - LeetHub

---
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates-greedy.py
@@ -0,0 +1,11 @@
+class Solution:
+    def buyChoco(self, prices: List[int], money: int) -> int:
+        min1 = min2 = float("inf")
+        for p in prices:
+            if p < min1:
+                min1, min2 = p, min1
+            elif p < min2:
+                min2 = p
+        change = money - min1 - min2
+        return change if change >= 0 else money
+        
