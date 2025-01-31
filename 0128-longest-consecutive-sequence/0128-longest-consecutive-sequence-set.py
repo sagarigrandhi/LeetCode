d662c964710cac6390596b3e3cc4461f24f9e305
Ratna Sagari Grandhi <sagari.grandhi@gmail.com>
2025-01-31 15:41:29 -0500

    Update 0128-longest-consecutive-sequence-set.py

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence-set.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence-set.py
@@ -2,7 +2,6 @@ class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
         max_length = 0
         visited = set(nums)
-        
         for num in nums:
             prev_num = num - 1
             if prev_num not in visited:
@@ -11,7 +10,5 @@ class Solution:
                 while next_num in visited:
                     length += 1
                     next_num += 1
-
                 max_length = max(length, max_length)
-        return max_length
-    
+        return max_length 
