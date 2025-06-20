7ba09bfe206c4410e6f13b31f91f5ac26d252fd6
Ratna Sagari Grandhi <sagari.grandhi@gmail.com>
2025-06-20 15:14:26 -0400

    Delete 0001-two-sum/0001-two-sum-dictionary.py

--- 0001-two-sum/0001-two-sum-dictionary.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        nums_dict = {}
-        for i in range(len(nums)):
-            if nums[i] in nums_dict:
-                return [i, nums_dict[nums[i]]]
-            else:
-                second = target - nums[i]
-                nums_dict[second] = i
